17aa815ce96ed635d46f0ad990c6240718b2f0cb
MZCretin <mxnzp_life@163.com>
2020-04-16 11:06:59 +0800

    两数之和

--- src/C1_两数之和.java
+++ src/C1_两数之和.java
@@ -1,9 +1,9 @@
 /**
  * Copyright (C), 2015-2020, XXX有限公司
- * FileName: W1_两数之和
+ * FileName: C1_两数之和
  * Author:   cretin
  * Date:     4/16/20 09:49
- * Description: W1_两数之和
+ * Description: C1_两数之和
  * History:
  * <author>          <time>          <version>          <desc>
  * 作者姓名           修改时间           版本号              描述
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 〈W1_两数之和〉
+ * 〈C1_两数之和〉
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  * <p>
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
@@ -28,7 +28,7 @@ import java.util.Map;
  * @create 4/16/20
  * @since 1.0.0
  */
-public class W1_两数之和 {
+public class C1_两数之和 {
     public static void main(String[] a) {
         int[] nums = new int[]{2, 7, 11, 15};
         int target = 9;
